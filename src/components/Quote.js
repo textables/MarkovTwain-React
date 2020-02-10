cd33bdfa56b17064996f36505766268aa0bbcaba
Joseph Tatum <josephtatum@gmail.com>
2020-02-10 13:35:22 -0800

    setup quote component basics

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Quote = ({ text, source }) => {
+  return (
+    <section>
+      <p>&quot;{text}&quot; - {source}</p>
+    </section>
+  );
+};
+
+Quote.propTypes = {
+  text: PropTypes.string.isRequired,
+  source: PropTypes.string.isRequired
+};
